b547431772e28a23a7d994b7ce6752b83fdfe365
Bara' Ayyash <baraayyash93@gmail.com>
2026-04-10 14:53:45 +0300

    Create 20260409062217-demo-user.js

---
+++ expressWithSequelizeCli/seeders/20260409062217-demo-user.js
@@ -0,0 +1,24 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'example@example.com',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        firstName: 'Ahmad ',
+        lastName: 'Ali',
+        email: 'ahmadali@example.com',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    ]);
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Users', null, {});
+  },
+};
+
